8fe4851a1c2fa774a3a70d56f0652c2d0d0cc098
c413bxd <caleb.895.9@gmail.com>
2019-04-11 11:41:05 -0500

    version1

---
+++ src/pages/home.vue
@@ -0,0 +1,55 @@
+<template>
+  <f7-page class="color-theme-orange">
+    <f7-navbar class="navbar color-theme-orange">
+      <f7-nav-left>
+        <f7-link icon-if-ios="f7:keyboard" icon-if-md="material:keyboard" panel-open="left"></f7-link>
+      </f7-nav-left>
+      <f7-nav-title>My App</f7-nav-title>
+      <f7-nav-right>
+        <f7-link icon-if-ios="f7:comment" icon-if-md="material:comment" panel-open="right"></f7-link>
+        <f7-button panel-open="right">Po</f7-button>
+      </f7-nav-right>
+    </f7-navbar>
+    <f7-toolbar>
+      <f7-link>Left Link</f7-link>
+      <f7-link>Right Link</f7-link>
+    </f7-toolbar>
+    <f7-block strong>
+      <p>Here is your blank Framework7 app. Let's see what we have here.</p>
+    </f7-block>
+    <f7-block-title>Navigation</f7-block-title>
+    <f7-list>
+      <f7-list-item link="/about/" title="About"></f7-list-item>
+      <f7-list-item link="/form/" title="Form"></f7-list-item>
+    </f7-list>
+    <f7-block-title>Modals</f7-block-title>
+    <f7-block strong class="color-theme-orange">
+      <f7-row>
+        <f7-col width="50">
+          <f7-button fill raised popup-open="#popup">Popup</f7-button>
+        </f7-col>
+        <f7-col width="50">
+          <f7-button fill raised login-screen-open="#login-screen">Login Screen</f7-button>
+        </f7-col>
+      </f7-row>
+    </f7-block>
+    <f7-block-title>Panels</f7-block-title>
+    <f7-block strong class="color-theme-orange">
+      <f7-row>
+        <f7-col width="50">
+          <f7-button fill raised panel-open="left">Left Panel</f7-button>
+        </f7-col>
+        <f7-col width="50">
+          <f7-button fill raised panel-open="right">Right Panel</f7-button>
+        </f7-col>
+      </f7-row>
+    </f7-block>
+    <f7-list>
+      <f7-list-item link="/dynamic-route/blog/45/post/125/?foo=bar#about" title="Dynamic Route"></f7-list-item>
+      <f7-list-item link="/load-something-that-doesnt-exist/" title="Default Route (404)"></f7-list-item>
+    </f7-list>
+  </f7-page>
+</template>
+<script>
+export default {}
+</script>
---
+++ src/pages/panel-left.vue
@@ -0,0 +1,31 @@
+<template>
+  <f7-page class="color-theme-red">
+    <f7-navbar title="Izquierda Panel"></f7-navbar>
+    <f7-block strong>
+      <p>Left panel content goes aqui</p>
+    </f7-block>
+    <f7-block-title>Load page in panel</f7-block-title>
+    <f7-list>
+      <f7-list-item link="/about/" title="About"></f7-list-item>
+      <f7-list-item link="/form/" title="Form"></f7-list-item>
+    </f7-list>
+    <f7-block-title>Load page in main view</f7-block-title>
+    <f7-list>
+      <f7-list-item link="/about/" title="About" view="#main-view" panel-close></f7-list-item>
+      <f7-list-item link="/panel-left/" title="Form" view="#main-view" panel-close></f7-list-item>
+      <f7-link href="/panel-left/"  view="#main-view">Nuevo</f7-link>
+    </f7-list>
+  </f7-page>
+</template>
+<script>
+import routes from '../routes.js'
+export default {
+   data() {
+    return {
+    
+        routes: routes,
+      }
+    }
+  }
+
+</script>
